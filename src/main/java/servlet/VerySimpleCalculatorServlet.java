5ffc501e4b5962b9c593e4c148f8e7ee475cc58b
Unknown <nuntuch13589@gmail.com>
2018-08-19 15:13:40 +0700

    Edit import package o verySimpleCalculatorServlet.java

--- src/main/java/servlet/VerySimpleCalculatorServlet.java
+++ src/main/java/servlet/VerySimpleCalculatorServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import sit.int303.first.model.SimpleCalculator;
+import model.SimpleCalculator;
 
 /**
  *
