f890933c0c51326cd0235f4f415914a86165e629
Unknown <nuntuch13589@gmail.com>
2018-08-19 15:38:46 +0700

    Edit Try On VerySimpleCalculatorServlet.java

--- src/main/java/servlet/VerySimpleCalculatorServlet.java
+++ src/main/java/servlet/VerySimpleCalculatorServlet.java
@@ -37,7 +37,8 @@ public class VerySimpleCalculatorServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        try  {
+            PrintWriter out = response.getWriter()
             /* TODO output your page here. You may use following sample code. */
             
             try {
