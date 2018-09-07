268274026d9d2f6339c80d79f867bc73549809f3
Unknown <nuntuch13589@gmail.com>
2018-09-07 18:23:37 +0700

    Edit SimpleCalculatorServlet.java

--- src/main/java/sit/int303/first/servlet/SimpleCalculatorServlet.java
+++ src/main/java/sit/int303/first/servlet/SimpleCalculatorServlet.java
@@ -32,7 +32,7 @@ public class SimpleCalculatorServlet extends HttpServlet {
 
         response.setContentType("text/html;charset=UTF-8");
 //        try (PrintWriter out = response.getWriter()) {
-        PrintWriter out = response.getWriter());
+        PrintWriter out = response.getWriter();
             /* TODO output your page here. You may use following sample code. */
 
             try {
